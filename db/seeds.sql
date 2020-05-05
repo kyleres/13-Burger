b914e4b7391bdf49107f1da1f1e9c25fa54a6818
Dexter <kyleres@live.com>
2020-05-04 19:25:21 -0500

    basic setup code

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+    ("Mushroom and Swiss", true),
+    ("Double Patty BLT", false),
+    ("Buffalo Chicken with Blue Cheese", false);
